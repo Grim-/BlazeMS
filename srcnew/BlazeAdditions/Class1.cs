c89afd2efcc119846887403da5f3c65975158ce2
Bry <pheonix2105@gmail.com>
2025-05-23 02:54:46 +0100

    wdwdiwdwdwdw

---
+++ srcnew/BlazeAdditions/Class1.cs
@@ -0,0 +1,40 @@
+﻿using NarutoMod;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaranMagicFramework;
+using Verse;
+
+namespace BlazeAdditions
+{
+    [StaticConstructorOnStartup]
+    public static class BlazePatch
+    {
+        static BlazePatch()
+        {
+            WN_Utils.mangekyouTrees.Add(BlazeDefOf.WN_MangekyouIZ);
+        }
+    }
+    [DefOf]
+    public static class BlazeDefOf
+    {
+        public static AbilityTreeDef WN_MangekyouIZ;
+    }
+
+    //public class BlazeNarutoAdditionsMod : Mod
+    //{
+    //    public BlazeNarutoAdditionsMod(ModContentPack content) : base(content)
+    //    {
+    //        WN_Utils.mangekyouTrees.Add(BlazeDefOf.WN_MangekyouIZ);
+    //    }
+    //}
+
+    //[DefOf]
+    //public static class BlazeDefOf
+    //{
+    //    public static AbilityTreeDef WN_MangekyouIZ;
+    //}
+}
